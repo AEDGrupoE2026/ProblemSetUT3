9ede8a8583e7863294164fae1197815e721c03f6
Agustín Montaña Larralde <agustinmont2001@gmail.com>
2026-05-14 21:07:23 -0300

    Métodos declarados

--- src/main/java/ucu/edu/aed/tda/trie/impl/Trie.java
+++ src/main/java/ucu/edu/aed/tda/trie/impl/Trie.java
@@ -1,7 +1,23 @@
 package ucu.edu.aed.tda.trie.impl;
 
 import ucu.edu.aed.tda.trie.TTrie;
+import java.util.function.Consumer;
+import java.util.ListM;
 
 public class Trie<T> implements TTrie<T> {
-    // declarar métodos de TTrie
+    
+
+    public Trie() {
+
+    }
+
+    @Override
+    public void recorrer(Consumer<Entry<T>> consumer) {
+        
+    }
+
+    @Override
+    public Entry<T> buscar(String palabra) {
+
+    }
 }
